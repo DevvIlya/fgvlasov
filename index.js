5553c56c78a9b63229a9b160b22630ad5f0e29e3
DevvIlya <yi1lei6a3@gmail.com>
2023-11-23 22:52:04 +0400

    added rep output

--- index.js
+++ index.js
@@ -1,39 +1,72 @@
 // index.js
 const Mustache = require('mustache');
 const fs = require('fs');
+const axios = require('axios');
 const MUSTACHE_MAIN_DIR = './main.mustache';
-/**
-  * DATA is the object that contains all
-  * the data to be provided to Mustache
-  * Notice the "name" and "date" property.
-*/
-let DATA = {
-  name: 'Fedor',
-  date: new Date().toLocaleDateString('en-GB', {
-    weekday: 'long',
-    month: 'long',
-    day: 'numeric',
-    hour: 'numeric',
-    minute: 'numeric',
-    timeZoneName: 'short',
-    timeZone: 'Europe/Moscow',
-  }),
-};
 
-// Добавляем дату последнего обновления
-DATA.refresh_date = new Date().toLocaleString('en-GB');
+// Получите токен доступа из переменных окружения
+const GITHUB_API_TOKEN = process.env.GITHUB_API_TOKEN;
 
+async function getTopRepositories() {
+  const username = 'fgvlasov';
 
-/**
-  * A - We open 'main.mustache'
-  * B - We ask Mustache to render our file with the data
-  * C - We create a README.md file with the generated output
-  */
+  // Получите репозитории для пользователя
+  const response = await axios.get(`https://api.github.com/fgvlasov/${username}/repos`, {
+    headers: {
+      Authorization: `Bearer ${GITHUB_API_TOKEN}`,
+    },
+  });
+
+  const repositories = response.data;
+
+  // Получите количество коммитов для каждого репозитория
+  const repoCommits = await Promise.all(
+    repositories.map(async (repo) => {
+      const commitsResponse = await axios.get(repo.commits_url.replace('{/sha}', ''), {
+        headers: {
+          Authorization: `Bearer ${GITHUB_API_TOKEN}`,
+        },
+      });
+
+      const commitCount = commitsResponse.data.length;
+
+      return { name: repo.name, commitCount };
+    })
+  );
+
+  // Сортируйте репозитории по количеству коммитов в порядке убывания
+  const sortedRepos = repoCommits.sort((a, b) => b.commitCount - a.commitCount);
+
+  // Возьмите два наиболее коммитированных репозитория
+  const topTwoRepos = sortedRepos.slice(0, 2);
+
+  return topTwoRepos;
+}
+
 async function generateReadMe() {
-  await fs.readFile(MUSTACHE_MAIN_DIR, (err, data) => {
+  const topRepos = await getTopRepositories();
+
+  // Объедините топовые репозитории с текущими данными
+  const DATA = {
+    name: 'Fedor',
+    date: new Date().toLocaleDateString('en-GB', {
+      weekday: 'long',
+      month: 'long',
+      day: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric',
+      timeZoneName: 'short',
+      timeZone: 'Europe/Moscow',
+    }),
+    refresh_date: new Date().toLocaleString('en-GB'),
+    top_repositories: topRepos,
+  };
+
+  fs.readFile(MUSTACHE_MAIN_DIR, (err, data) => {
     if (err) throw err;
     const output = Mustache.render(data.toString(), DATA);
     fs.writeFileSync('README.md', output);
   });
 }
+
 generateReadMe();
